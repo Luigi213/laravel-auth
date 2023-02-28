a08ce3ea026f7c30492577a2a1c8e141405f3b47
Luigi213 <reyesgianluigi88@gmail.com>
2023-02-28 13:35:39 +0100

    seeder done and andd admin layout and folder admin

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Models\Project;
+use Faker\Generator as Faker;
 
 class ProjectSeeder extends Seeder
 {
@@ -12,8 +15,15 @@ class ProjectSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        $newProject = new Project();
+
+        for($i=0; $i<10; $i++){
+            $newProject->titolo = Str::slug($faker->name(), '-');
+            $newProject->descrizione = $faker->paragraph(2);
+
+            $newProject->save();
+        }
     }
 }
