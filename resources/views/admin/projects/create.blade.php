d6779d949c06437e92fbe59d83e5a46f319fd279
Luigi213 <reyesgianluigi88@gmail.com>
2023-02-28 15:23:46 +0100

    completed and add bonus create store

---
+++ resources/views/admin/projects/create.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-12">
+                
+            </div>
+        </div>
+    </div>
+@endsection
